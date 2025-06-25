68ca04d6df09255831b138bbb50ac1447329f446
Andrés <andrestacuaman13@gmail.com>
2025-06-25 08:29:18 -0500

    DiagnosisDTO

---
+++ SMED.Shared/DTOs/DiagnosisDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMED.Shared.DTOs
+{
+    public class DiagnosisDTO
+    {
+        public int Id { get; set; }
+        public string Cie10 { get; set; } = null!;
+        public string Denomination { get; set; } = null!;
+        public string DiagnosticType { get; set; } = null!;
+        public string Recurrence { get; set; } = null!;
+        public string DiagnosisMotivation { get; set; } = null!;
+        public int MedicalCareId { get; set; }
+        public int? DiseaseId { get; set; }
+
+        public string? DiseaseName { get; set; }
+        public List<int>? OrderIds { get; set; }
+        public List<int>? TreatmentIds { get; set; }
+        public List<int>? InterconsultationIds { get; set; }
+    }
+
+}

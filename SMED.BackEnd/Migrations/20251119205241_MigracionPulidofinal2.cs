6440d11bce9b8cfe5df288bf392a88653027c1e3
LuisSancho64 <kidbucle100@gmail.com>
2025-12-10 12:07:17 -0500

    Migraciones

---
+++ SMED.BackEnd/Migrations/20251119205241_MigracionPulidofinal2.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SMED.BackEnd.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigracionPulidofinal2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MedicalReferrals_Locations_LocationId",
+                table: "MedicalReferrals");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "LocationId",
+                table: "MedicalReferrals",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MedicalReferrals_Locations_LocationId",
+                table: "MedicalReferrals",
+                column: "LocationId",
+                principalTable: "Locations",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MedicalReferrals_Locations_LocationId",
+                table: "MedicalReferrals");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "LocationId",
+                table: "MedicalReferrals",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MedicalReferrals_Locations_LocationId",
+                table: "MedicalReferrals",
+                column: "LocationId",
+                principalTable: "Locations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
